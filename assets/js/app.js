3015235022c730c366d52c39809f6dde243bbd8e
Verónica Del Valle <139937653+veronicadelvalle@users.noreply.github.com>
2023-10-26 21:21:06 -0300

    Add files via upload
    portafolio actualizado

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/* <div>
+    <h3>title</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt et, sed dignissimos fugiat
+      necessitatibus, quia sunt fugit non recusandae asperiores possimus eaque aliquam.</p>
+    <ul class="list-lenguage">
+      <li><i class="fab fa-html5"></i></li>
+      <li><i class="fab fa-css3"></i></li>
+      <li><i class="fab fa-js"></i></li>
+    </ul>
+  </div> 
+*/
